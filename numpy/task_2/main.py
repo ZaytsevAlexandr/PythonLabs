0fda074db108cc66a2a51224444752147b29d1bd
Zaytsev Aleksandr <145544514+ZaytsevAlexandr@users.noreply.github.com>
2024-12-03 21:18:57 +0300

    Внесение правок

--- numpy/task_2/main.py
+++ numpy/task_2/main.py
@@ -1,28 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read(name):
-    return np.loadtxt(name)
-
 def filter(data):
-    cumsum_data = np.cumsum(data)
-    filtred_data = np.zeros_like(data)
-    filtred_data[:9] = cumsum_data[:9] / (np.arange(1, 10))
-    filtred_data[9:] = (cumsum_data[9:] - cumsum_data[:-9]) / 10
+    filtred_data = np.array([])
+    for i in range(len(data)):
+        if i < 9:
+            filtred_data = np.append(filtred_data, np.average(data[0:(i+1)]))
+        else:
+            filtred_data = np.append(filtred_data, np.average(data[i-9:(i+1)]))
     return filtred_data
 
 def draw(data, filtred_data, save_name):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))  # 1 ряд, 2 столбца
     ax1.plot(data)
     ax1.set_title('Original Data')
     ax2.plot(filtred_data)
     ax2.set_title('Filtered Data')
-    plt.tight_layout()
+    plt.tight_layout() 
     plt.savefig(save_name)
     plt.show()
 
 def complete_task(file_name, save_name):
-    data = read(file_name)
+    data = np.loadtxt(file_name)
     filtred_data = filter(data)
     draw(data, filtred_data, save_name)
 
